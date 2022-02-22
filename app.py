1fe6f8ad37e62b7ffbeecd179854a12f8417fd56
Fan-R <rf96@duke.edu>
2022-02-22 11:26:49 -0500

    added front-end version into app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template,url_for
 import pickle
 
 app = Flask(__name__)
+version = "3.0"
 
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
@@ -11,6 +12,11 @@ with open('model.pkl', 'rb') as f:
 
 tf_transformer = pickle.load(open('tfidf1.pkl','rb'))
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# for CLI
 @app.route('/api/american',methods=['POST'])
 def predict():
     tfidf = TfidfVectorizer(stop_words = "english",vocabulary = tf_transformer.vocabulary_)
@@ -20,10 +26,18 @@ def predict():
     feature_vect = tfidf.fit_transform(feature)
 
     prediction = model.predict(feature_vect)[0]
-    version = "1.0"
-    # model_date = "XXX"
 
     return jsonify({"is_american ":int(prediction),"version":version,"model_date":date})
+
+# front-end version
+@app.route('/api/american/html',methods=['POST'])
+def html_predict():
+    tfidf = TfidfVectorizer(stop_words = "english",vocabulary = tf_transformer.vocabulary_)
+    feature = list(request.form['text'])
+    feature_vect = tfidf.fit_transform(feature)
+    prediction = model.predict(feature_vect)[0]
+
+    return render_template('index.html', prediction="is_american: {}".format(prediction),version = "version: {}".format(version),date = "model_date: {}".format(date))
 
 if __name__ == "__main__":
     app.run(debug=True)
